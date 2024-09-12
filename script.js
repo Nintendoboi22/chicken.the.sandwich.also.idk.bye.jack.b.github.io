2e80dc37313745d459dadf49b43d1281ab18e50d
Nintendoboi22 <131544102+Nintendoboi22@users.noreply.github.com>
2024-09-12 10:25:44 -0500

    seeeeeeeeeeeeeeeee

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ let dealerHand = [];
 let deck = [];
 
 function createDeck() {
-    const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
     const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     deck = [];
-    for (let suit of suits) {
-        for (let value of values) {
-            deck.push({ suit, value });
+    for (let value of values) {
+        for (let i = 0; i < 4; i++) { // 4 suits, but we won't display them
+            deck.push({ value });
         }
     }
 }
@@ -27,8 +26,6 @@ function startGame() {
     shuffleDeck();
     playerHand = [deck.pop(), deck.pop()];
     dealerHand = [deck.pop(), deck.pop()];
-    bet = 100; // Example bet
-    balance -= bet;
     updateUI();
 }
 
@@ -36,12 +33,27 @@ function updateUI() {
     document.getElementById('balance').innerText = balance;
     document.getElementById('bet').innerText = bet;
     document.getElementById('player-hand').innerText = handToString(playerHand);
-    document.getElementById('dealer-hand').innerText = handToString(dealerHand);
+    document.getElementById('dealer-hand').innerText = dealerHandToString(dealerHand);
 }
 
 function handToString(hand) {
-    return hand.map(card => `${card.value} of ${card.suit}`).join(', ');
+    return hand.map(card => card.value).join(', ');
 }
+
+function dealerHandToString(hand) {
+    return `${hand[0].value}, [Hidden]`;
+}
+
+document.getElementById('place-bet').addEventListener('click', () => {
+    const betAmount = parseInt(document.getElementById('bet-amount').value);
+    if (betAmount > 0 && betAmount <= balance) {
+        bet = betAmount;
+        balance -= bet;
+        startGame();
+    } else {
+        alert('Invalid bet amount');
+    }
+});
 
 document.getElementById('hit').addEventListener('click', () => {
     playerHand.push(deck.pop());
@@ -54,10 +66,14 @@ document.getElementById('stand').addEventListener('click', () => {
 });
 
 document.getElementById('double-down').addEventListener('click', () => {
-    bet *= 2;
-    balance -= bet / 2;
-    playerHand.push(deck.pop());
-    updateUI();
+    if (balance >= bet) {
+        bet *= 2;
+        balance -= bet / 2;
+        playerHand.push(deck.pop());
+        updateUI();
+    } else {
+        alert('Not enough balance to double down');
+    }
 });
 
 document.getElementById('split').addEventListener('click', () => {
